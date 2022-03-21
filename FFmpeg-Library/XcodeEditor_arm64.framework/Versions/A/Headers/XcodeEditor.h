68d8c77ec5a1683a94dd1b48f144203b8f5cdf29
Pierre Boué <37743737+Macaplix@users.noreply.github.com>
2022-03-21 14:13:13 +0100

    x86 update

---
+++ FFmpeg-Library/XcodeEditor_arm64.framework/Versions/A/Headers/XcodeEditor.h
@@ -0,0 +1,39 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+//  JASPER BLUES
+//  Copyright 2012 Jasper Blues
+//  All Rights Reserved.
+//
+//  NOTICE: Jasper Blues permits you to use, modify, and distribute this file
+//  in accordance with the terms of the license agreement accompanying it.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for XcodeEditor.
+FOUNDATION_EXPORT double XcodeEditorVersionNumber;
+
+//! Project version string for XcodeEditor.
+FOUNDATION_EXPORT const unsigned char XcodeEditorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XcodeEditor/PublicHeader.h>
+
+
+#import <XcodeEditor/XCAbstractDefinition.h>
+#import <XcodeEditor/XCGroup.h>
+#import <XcodeEditor/XCClassDefinition.h>
+#import <XcodeEditor/XCFileOperationQueue.h>
+#import <XcodeEditor/XCFrameworkDefinition.h>
+#import <XcodeEditor/XCProject.h>
+#import <XcodeEditor/XCSourceFile.h>
+#import <XcodeEditor/XCSourceFileDefinition.h>
+#import <XcodeEditor/XCSubProjectDefinition.h>
+#import <XcodeEditor/XCTarget.h>
+#import <XcodeEditor/XCXibDefinition.h>
+#import <XcodeEditor/XCKeyBuilder.h>
+#import <XcodeEditor/XCProject+SubProject.h>
+#import <XcodeEditor/XCProjectBuildConfig.h>
+#import <XcodeEditor/XCBuildShellScript.h>
+#import <XcodeEditor/XCBuildShellScriptDefinition.h>
+#import <XcodeEditor/XCVersionGroup.h>
